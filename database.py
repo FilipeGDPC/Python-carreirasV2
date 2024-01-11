1ff7fdbce1c612f00bd6d72580ac4634ecd966c7
FilipeGDPC <99835864+FilipeGDPC@users.noreply.github.com>
2024-01-11 00:05:20 +0000

    switch port to eng language

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-string_conexao = "mysql+pymysql://xqftj4cfhldtamtybhj5:pscale_pw_z9YICD2BRR5t5gbbyvRHgPkp949uvSrT6Waiyu2nlPb@aws.connect.psdb.cloud/carreiras-python?charset=utf8mb4"
+string_conexao = "mysql+pymysql://dk1237r0wncsx31wpejq:pscale_pw_1XDJi9TCPQxf0tg6bSjM4hPFQUoH0GRf0EIDfuZFJjw@aws.connect.psdb.cloud/carreiras-python?charset=utf8mb4"
 engine = create_engine(string_conexao,
                        connect_args={"ssl": {
                            "ssl_ca": "/etc/ssl/cert.pem"
