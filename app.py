788ebef5a3fef9159c56ab55b6520b196633c74e
FilipeGDPC <99835864+FilipeGDPC@users.noreply.github.com>
2024-01-11 00:59:12 +0000

    final improvements

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
-from database import carrega_vagas_db
+from database import adiciona_inscricao, carrega_vaga_db, carrega_vagas_db
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    vagas = carrega_vagas_db()
-    return render_template("home.html", vagas=vagas)
+  vagas = carrega_vagas_db()
+  return render_template("home.html", vagas=vagas)
 
 
 @app.route("/vagas")
 def lista_vagas():
-    vagas = carrega_vagas_db()
-    return jsonify(vagas)
+  vagas = carrega_vagas_db()
+  return jsonify(vagas)
+
+
+@app.route("/vaga/<id>")
+def mostra_vaga(id):
+  vaga = carrega_vaga_db(id)
+  if not vaga:
+    return "Not Found", 404
+  return render_template("detalhevaga.html", vaga=vaga)
+
+
+@app.route("/vaga/<id>/inscricao", methods=["GET", "POST"])
+def inscricao_vaga(id):
+  vaga = carrega_vaga_db(id)
+  data = request.form
+  adiciona_inscricao(id, data)
+  return render_template("inscricao_concluida.html", vaga=vaga, inscricao=data)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,6 +1,9 @@
+
+import os
+
 from sqlalchemy import create_engine, text
 
-string_conexao = "mysql+pymysql://dk1237r0wncsx31wpejq:pscale_pw_1XDJi9TCPQxf0tg6bSjM4hPFQUoH0GRf0EIDfuZFJjw@aws.connect.psdb.cloud/carreiras-python?charset=utf8mb4"
+string_conexao = os.environ['DB_CONEXAO_STRING']
 engine = create_engine(string_conexao,
                        connect_args={"ssl": {
                            "ssl_ca": "/etc/ssl/cert.pem"
@@ -13,3 +16,27 @@ def carrega_vagas_db():
         for vaga in resultado.all():
           vagas.append(vaga._asdict())
         return vagas
+
+def carrega_vaga_db(id):
+  with engine.connect() as conn:
+    resultado = conn.execute(text("SELECT * FROM vagas WHERE id = :val"), {"val": id})
+    registro = resultado.mappings().all()
+    if len(registro) == 0:
+      return None
+    else:
+      return dict(registro[0])
+
+
+def adiciona_inscricao(id_vaga, dados):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO inscricoes(vaga_id, nome, email, linkedin, experiencia) VALUES(:vaga_id, :nome, :email, :linkedin, :experiencia)"
+    )
+    conn.execute(
+      query, {
+        'vaga_id': id_vaga,
+        'nome': dados['nome'],
+        'email': dados['email'],
+        'linkedin': dados['linkedin'],
+        'experiencia': dados['experiencia']
+      })
